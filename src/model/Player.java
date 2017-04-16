b88909e2bcc6f54568cbf7df882cfe80785eb40f
OleksiyNosov <oleksiynosov@gmail.com>
2017-04-16 18:00:03 +0300

    Add color to know which pieces is belons to Player

--- src/model/Player.java
+++ src/model/Player.java
@@ -2,12 +2,18 @@ package model;
 
 public class Player {
     private int id;
+    private int color;
 
-    public Player() {
+    public Player(int color) {
         id = UniquePlayerId.newId();
+        this.color = color;
     }
 
     public int getId() {
         return id;
+    }
+
+    public int getColor() {
+        return color;
     }
 }
