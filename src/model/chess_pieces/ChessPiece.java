2295aff01874c0c2adbdc71504ae09cc4127cd62
OleksiyNosov <oleksiynosov@gmail.com>
2017-04-16 17:51:55 +0300

    ChessPiece no longer have ChessPieceColor because it gets color it's color from Player and implement ChessPieceMomentLogic

--- src/model/chess_pieces/ChessPiece.java
+++ src/model/chess_pieces/ChessPiece.java
@@ -1,33 +1,36 @@
 package model.chess_pieces;
 
+import model.Board;
+import model.BoardPosition;
 import model.Player;
 
-public abstract class ChessPiece implements ChessPieceColor {
+public abstract class ChessPiece implements ChessPieceMovementLogic {
 
     private Player player;
-    private int color;
 
-    public ChessPiece(Player player, int color) {
+    public ChessPiece(Player player) {
         this.player = player;
-        this.color = color;
-    }
-
-    @Override
-    public void setColor(int color) {
-        this.color = color;
-    }
-
-    @Override
-    public int getColor() {
-        return color;
     }
 
     @Override
     public int hashCode() {
         int result = 15;
         result = 33 * result + getClass().hashCode();
-        result = 33 * result + getColor();
-        result = 33 * result + result + player.getId();
+        result = 33 * result + player.getColor();
+        result = 33 * result + player.getId();
         return result;
+    }
+
+    public boolean isEnemyFor(ChessPiece chessPiece) {
+        return chessPiece.getColor() != getColor();
+    }
+
+    public int getColor() {
+        return player.getColor();
+    }
+
+    @Override
+    public boolean checkIfPossible(Board board, BoardPosition place, BoardPosition destination) {
+        return getAllPossibleMovements(board, place).contains(destination);
     }
 }
