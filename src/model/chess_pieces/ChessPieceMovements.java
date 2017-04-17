ab1b3ba9a8743bc9bf445891c89ef4023d87ec74
OleksiyNosov <oleksiynosov@gmail.com>
2017-04-17 21:09:33 +0300

    Re-factor to be more simple and understandable, also it's no longer static

--- src/model/chess_pieces/ChessPieceMovements.java
+++ src/model/chess_pieces/ChessPieceMovements.java
@@ -3,130 +3,96 @@ package model.chess_pieces;
 import model.Board;
 import model.BoardPosition;
 
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ChessPieceMovements {
-    private static final int LEFT = -1;
-    private static final int RIGHT = 1;
+    public static final int LEFT   = -1;
+    public static final int RIGHT  =  1;
+    public static final int UP     = -1;
+    public static final int DOWN   =  1;
+    public static final int CENTER =  0;
 
-    private static final int UP = -1;
-    private static final int DOWN = 1;
+    private Board board;
+    private BoardPosition place;
+    private ChessPiece chessPiece;
+    private int max;
 
-    private static final int CENTER = 0;
+    private List<BoardPosition> moves;
 
+    public ChessPieceMovements(Board board, BoardPosition place, int max) {
+        this.board = board;
+        this.place = place;
+        this.chessPiece = board.getCellAt(place).getChessPiece();
+        this.max = max;
+        this.moves = new LinkedList<>();
+    }
 
-    private static Board board;
-    private static ChessPiece currentChessPiece;
-    private static BoardPosition place;
-
-    private ChessPieceMovements() {}
-
-    public static List<BoardPosition> getAllMovesForSides(Board board, BoardPosition place, int max) {
-        setFields(board, place);
-        List<BoardPosition> moves = new LinkedList<>();
-
-        moves.addAll(getAllPossibleMovesForLine(RIGHT,  CENTER, max));
-        moves.addAll(getAllPossibleMovesForLine(CENTER, DOWN,   max));
-        moves.addAll(getAllPossibleMovesForLine(LEFT,   CENTER, max));
-        moves.addAll(getAllPossibleMovesForLine(CENTER, UP,     max));
-
+    public List<BoardPosition> getMoves() {
         return moves;
     }
 
-    public static List<BoardPosition> getAllMovesForDiagonals(Board board, BoardPosition place, int max) {
-        setFields(board, place);
-        List<BoardPosition> moves = new LinkedList<>();
-
-        moves.addAll(getAllPossibleMovesForLine(RIGHT, DOWN, max));
-        moves.addAll(getAllPossibleMovesForLine(RIGHT, UP,   max));
-        moves.addAll(getAllPossibleMovesForLine(LEFT,  DOWN, max));
-        moves.addAll(getAllPossibleMovesForLine(LEFT,  UP,   max));
-
-        return moves;
+    public void calcMovesForSides() {
+        calcMovesForLine(RIGHT,  CENTER);
+        calcMovesForLine(CENTER, DOWN);
+        calcMovesForLine(LEFT,   CENTER);
+        calcMovesForLine(CENTER, UP);
     }
 
-    public static List<BoardPosition> getAllMovesForKnight(Board board, BoardPosition place) {
-        setFields(board, place);
-        List<BoardPosition> moves = new LinkedList<>();
-
-        moves.addAll(getAllPossibleMovesForLine(RIGHT * 2, DOWN, 1));
-        moves.addAll(getAllPossibleMovesForLine(RIGHT * 2, UP,   1));
-        moves.addAll(getAllPossibleMovesForLine(LEFT  * 2, DOWN, 1));
-        moves.addAll(getAllPossibleMovesForLine(LEFT  * 2, UP,   1));
-
-        moves.addAll(getAllPossibleMovesForLine(RIGHT,DOWN * 2, 1));
-        moves.addAll(getAllPossibleMovesForLine(RIGHT,UP   * 2, 1));
-        moves.addAll(getAllPossibleMovesForLine(LEFT, DOWN * 2, 1));
-        moves.addAll(getAllPossibleMovesForLine(LEFT, UP   * 2, 1));
-
-        return moves;
+    public void calcMovesForDiagonals() {
+        calcMovesForDiagonals(1, 1);
     }
 
-
-
-    public static List<BoardPosition> getAllMovesForPawn(Board board, BoardPosition place, int max, int direction) {
-        setFields(board, place);
-        List<BoardPosition> moves = new LinkedList<>();
-
-        moves.addAll(getOneOnlyForCombat(RIGHT, direction));
-        moves.addAll(getOneOnlyForCombat(LEFT,  direction));
-
-        moves.addAll(getOnlyForMove(CENTER, direction, max));
-
-        return moves;
+    public void calcMovesForKnight() {
+        calcMovesForDiagonals(2, 1);
+        calcMovesForDiagonals(1, 2);
     }
 
+    public void calcMovesForPawn(int direction) {
+        calcOneOnlyForCombat(RIGHT, direction);
+        calcOneOnlyForCombat(LEFT,  direction);
 
-    private static List<BoardPosition> getOneOnlyForCombat(int shiftByX, int shiftByY) {
-        LinkedList<BoardPosition> moves = new LinkedList<>();
+        calcOnlyForMove(CENTER, direction);
+    }
+
+    private void calcOneOnlyForCombat(int shiftByX, int shiftByY) {
         BoardPosition tempPos = new BoardPosition(place.getX() + shiftByX, place.getY() + shiftByY);
 
         if (board.isOnBoard(tempPos) && ( ! board.getCellAt(tempPos).isEmpty()))
-            if (board.getCellAt(tempPos).getChessPiece().isEnemyFor(currentChessPiece))
+            if (board.getCellAt(tempPos).getChessPiece().isEnemyFor(chessPiece))
                 moves.add(tempPos);
-
-        return moves;
     }
 
-    private static List<BoardPosition> getOnlyForMove(int shiftByX, int shiftByY, int max) {
-        LinkedList<BoardPosition> moves = new LinkedList<>();
-
+    private void calcOnlyForMove(int shiftByX, int shiftByY) {
         for (int i = 1; i <= max; i++) {
             BoardPosition tempPos = new BoardPosition(place.getX() + shiftByX * i, place.getY() + shiftByY * i);
             if (board.isOnBoard(tempPos) && board.getCellAt(tempPos).isEmpty())
                 moves.add(tempPos);
         }
-
-        return moves;
     }
 
-    private static LinkedList<BoardPosition> getAllPossibleMovesForLine(int shiftByX, int shiftByY, int max) {
-        LinkedList<BoardPosition> moves = new LinkedList<>();
-
+    private void calcMovesForLine(int shiftByX, int shiftByY) {
         for (int i = 1; i <= max; i++) {
             BoardPosition tempPos = new BoardPosition(place.getX() + shiftByX * i, place.getY() + shiftByY * i);
             if (board.isOnBoard(tempPos)) {
                 if (board.getCellAt(tempPos).isEmpty()) {
                     moves.add(tempPos);
-                } else if (board.getCellAt(tempPos).getChessPiece().isEnemyFor(currentChessPiece)) {
+                } else if (board.getCellAt(tempPos).getChessPiece().isEnemyFor(chessPiece)) {
                     moves.add(tempPos);
-                    return moves;
+                    return;
                 } else  {
-                    return moves;
+                    return;
                 }
             } else {
-                return moves;
+                return;
             }
         }
-
-        return moves;
     }
 
-    private static void setFields(Board board, BoardPosition place) {
-        ChessPieceMovements.board = board;
-        ChessPieceMovements.currentChessPiece = board.getCellAt(place).getChessPiece();
-        ChessPieceMovements.place = place;
+    private void calcMovesForDiagonals(int xMultiplier, int yMultiplier) {
+        calcMovesForLine(xMultiplier * RIGHT, yMultiplier * DOWN);
+        calcMovesForLine(xMultiplier * RIGHT, yMultiplier * UP);
+        calcMovesForLine(xMultiplier * LEFT,  yMultiplier * DOWN);
+        calcMovesForLine(xMultiplier * LEFT,  yMultiplier * UP);
     }
 }
